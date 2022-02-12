a4a7d419e9529156ba49579e711a70dffa1c3cd6
xiufus <18516306381@163.com>
2022-02-12 21:25:55 +0800

    提交新文件Test006.java

---
+++ demo01/src/com/xiufus/test/Test006.java
@@ -0,0 +1,12 @@
+package com.xiufus.test;
+
+/**
+ * @description:
+ * @author: sangx
+ * @time: 2022/2/12 21:25
+ */
+public class Test006 {
+    public static void main(String[] args) {
+        System.out.println("5678787698");
+    }
+}
