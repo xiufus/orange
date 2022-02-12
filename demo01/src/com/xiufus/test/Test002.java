0898b8aa4a2aa398264825e6045c70c2fbb7aede
xiufus <18516306381@163.com>
2022-02-12 19:51:05 +0800

    提交新文件678
    Signed-off-by: xiufus <18516306381@163.com>

---
+++ demo01/src/com/xiufus/test/Test002.java
@@ -0,0 +1,12 @@
+package com.xiufus.test;
+
+/**
+ * @description:
+ * @author: sangx
+ * @time: 2022/2/12 19:48
+ */
+public class Test002 {
+    public static void main(String[] args) {
+        System.out.println("r##################");
+    }
+}
