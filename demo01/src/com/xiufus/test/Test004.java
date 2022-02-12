8b6563f6e6ee7a693ce0f4491003e0ef39d84724
xiufus <18516306381@163.com>
2022-02-12 21:21:14 +0800

    更新文件Test004.java

--- demo01/src/com/xiufus/test/Test004.java
+++ demo01/src/com/xiufus/test/Test004.java
@@ -6,4 +6,7 @@ package com.xiufus.test;
  * @time: 2022/2/12 21:16
  */
 public class Test004 {
+    public static void main(String[] args) {
+        System.out.println("23456##################");
+    }
 }
